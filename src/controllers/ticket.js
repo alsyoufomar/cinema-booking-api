94ee46e70c48cfccb5afd4b4976665ba2ca8acc7
Omar Alsyouf <omaralsoyouf@gmail.com>
2022-04-05 09:46:01 +0100

    the ticket route should send back the ticket details

--- src/controllers/ticket.js
+++ src/controllers/ticket.js
@@ -3,13 +3,14 @@ const prisma = require('../utils/prisma');
 
 const createTicket = (req, res) => {
   const { screeningId, customerId } = req.body;
-  // let options = { create: { movieId, startsAt } }
-  // if (movieId === undefined) options = {}
 
   prisma.ticket.create({
     data: {
       screeningId,
       customerId
+    },
+    include: {
+      screening: true
     }
   })
     .then(screen => res.json({ data: screen }))
